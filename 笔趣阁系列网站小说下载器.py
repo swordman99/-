3c518c03eb1718228457c4735eaee3104728e043
swordman99 <39516706+swordman99@users.noreply.github.com>
2019-01-21 21:38:21 +0800

    v2.0

--- 笔趣阁系列网站小说下载器.py
+++ 笔趣阁系列网站小说下载器.py
@@ -15,13 +15,17 @@ else:
 	code=input('请输入网页编码格式（例如GB2312）')
 req.encoding=code
 html=BeautifulSoup(req.text, features="html.parser")
+# 查找章节名，章节链接
 temp_1=html.find_all('dd')
 a=BeautifulSoup(str(temp_1), features="html.parser").find_all('a')
+# 查找小说名，作者名
 info=html.find_all('div', id='info')
 temp_2=BeautifulSoup(str(info), features="html.parser")
 book_title=temp_2.find_all('h1')[0].text
 writer=temp_2.find_all('p')[0].text.replace('\xa0','')
-with open('%s\\%s.txt'%(os.path.dirname(os.path.realpath(__file__)),book_title),'a',encoding='utf-8') as f:
+# 生成文件路径
+path_file = '%s\\%s.txt'%(os.path.dirname(os.path.realpath(__file__)),book_title)
+with open(path_file,'a',encoding='utf-8') as f:
 	f.write(book_title+'\n'+writer+'\n')
 print("正在下载 "+book_title,end=' ')
 print(writer)
@@ -37,15 +41,21 @@ for i in range(len(a)):
 		href.append(URL+a[i].get('href'))
 	title.append(a[i].text)
 print('标题获取完成')
+# 判断是否有“最新章节”，若有则去除重复
+N_total=0
+start=0 # 过滤掉开始的重复部分
 flag=len(html.find_all('dt'))
 if flag == 2:
-	print('小说共%d章'%(len(a)-9))
-	i=9
+	N_total = len(a)-9
+	start = 9
+	print('小说共%d章'% N_total)
 else:
-	print('小说共%d章'%(len(a)))
-	i=0
+	N_total = len(a)
+	start = 0
+	print('小说共%d章'% N_total)
+i = start
 while i < len(href):
-	sys.stdout.write("已下载%.3f%%"%((i+1)*100/len(href))+'\r')
+	sys.stdout.write("已下载%.3f%%"%((i-start+1)*100/N_total)+'\r')
 	sys.stdout.flush()
 	URL_each=href[i]
 	req=requests.get(url=URL_each)
@@ -53,14 +63,13 @@ while i < len(href):
 	html_each=BeautifulSoup(req.text, features="html.parser")
 	texts_each=html_each.find_all('div', id='content')
 	if len(texts_each)>0:
-		temp_3=texts_each[0].text.replace('\xa0','')
-		temp_3=temp_3.replace('  ','\n')
-		temp_3=temp_3.replace('　　','\n')
-		temp_3=temp_3.replace('\n\n','\n')
-		with open('%s\\%s.txt'%(os.path.dirname(os.path.realpath(__file__)),book_title),'a',encoding='utf-8') as f:
+		temp_3=texts_each[0].text.replace('\xa0','\n')
+		temp_3=texts_each[0].text.replace('  ','\n')
+		temp_3=texts_each[0].text.replace('　　','\n')
+		with open(path_file,'a',encoding='utf-8') as f:
 			f.write('\n'+title[i]+'\n'+temp_3)
 		i=i+1
-	else:
+	else: # 防止应网络不畅通造成的错误
 		pass
 print('\n下载完成')
 input()
